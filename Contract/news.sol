96900d0e005b3aed9ae72b6308e44fede33ca296
HARSH2011\Harsh <harsh20111997@gmail.com>
2018-03-03 17:55:44 +0530

    added multiple news feacture

--- Contract/news.sol
+++ Contract/news.sol
@@ -2,38 +2,62 @@ pragma solidity ^0.4.0;
 
 contract mortal
 {
+    
     address public owner;
     
     function mortal()
     {
         owner = msg.sender;
     }
-    function kill()
-    {
-        suicide(owner);
+    function kill () internal {
+        selfdestruct(owner);
     }
+    
 }
 
 contract NewsContract is mortal
 {
-    string public storedData;
-    string public authorName;
-    string public Tag;
     
-    struct Voters
+    string private storedData;
+    string private authorName;
+    string private Tag;
+    uint private negativeVote;
+    struct Voter
     {
         address voterAddress;
-        bool Voted;
+        bool voted;
     }
+    
     
     mapping(address => Voter) public voters;
     
     
     function NewsContract(string _storedData,string _authorName,string _Tag) public
     {
+        
         storedData = _storedData;
         authorName = _authorName;
         Tag = _Tag;
+        negativeVote = 0;
+        
+    }
+    
+    function vote() public 
+    {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted);
+        sender.voted = true;
+        negativeVote++;
+        if (negativeVote >= 2)
+        {
+            kill();
+        }
+    }
+    
+    function getVote() public view
+        returns ( uint negativeVote_)
+    {
+        negativeVote_ = negativeVote;
     }
     
     function returnNews() public view
@@ -43,8 +67,7 @@ contract NewsContract is mortal
             authorName_ = authorName;
             Tag_ = Tag;
         }
-    
-    
+        
 }
 
 
